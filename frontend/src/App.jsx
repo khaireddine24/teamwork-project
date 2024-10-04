2773aabe39f10d8359b762694fdfbda37ce77fbb
khaireddine <ihrissanek@gmail.com>
2024-10-04 15:42:54 +0100

    FrontEnd intializer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+
+import {useEffect, useState} from 'react'
+
+const App=()=> {
+  {/* Hooks (useState(),useEffect(),useRef(),useReducer(),useMemo() ...) */}
+  const [name,setName]=useState('');
+  const [date,setDate]=useState(null);
+  useEffect(()=>{
+    giveDate();
+  },[]);
+
+  {/* Arrow Function */}
+  const giveDate=()=>{
+    setDate(new Date().getFullYear());
+  }
+
+  if(!name){
+    setName('TeamWork');
+  }
+  return (
+    <div className='bg-slate-400 max-w-[768px] ml-[30%]'>
+      <h1 className='lg:text-red-600 text-center font-bold text-5xl mt-[90px] sm:text-blue-700 border-4 rounded-lg'>Date {date} </h1>
+      <p className='text-white text-center text-3xl p-5'>welcome {name}</p>
+    </div>
+  )
+}
+
+export default App
